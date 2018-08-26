18ebd5d85a4dd72fbff3527e674645d92199d06a
newbietogit <newbietogit@gmail.com>
2018-08-26 16:20:10 +0530

    added menu
    note :-
    1. change font type and size
    2. change color platten

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<title>Cloud</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/menu.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+	<!-- Including Menu File --> 
+	<?php require('menu.php'); ?>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<header>
+	<div class="logo">
+		<div class="top"></div>
+		<div class="base"></div>
+	</div>
+	<nav>
+		<ul>
+			<li><a href="#">Plans &amp; Pricing</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Feedback</a></li>
+			<li><a href="#">Login</a></li>
+			<li><a href="#">Create Account</a></li>
+		</ul>
+	</nav>
+</header>
